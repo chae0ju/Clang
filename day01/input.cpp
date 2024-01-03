01794e0752c3eadc0b6e53f187d6dcbce40c93fe
chae0ju <155497582+chae0ju@users.noreply.github.com>
2024-01-03 10:49:10 +0900

    Add files via upload

---
+++ day01/input.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+
+	//º¯¼ö(variable)
+	int mega = 123;
+	printf("%d", mega);
+
+	return 0;
+}
---
+++ day01/output.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+	printf("채영은,20,소프트웨어학과");
+	return 0;
+}
